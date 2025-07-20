70b5a80a3f9f9087f5d9d0fd822d4fb31ced6b60
Mohit <mohitkhairnar28@gmail.com>
2025-07-20 23:37:22 +0530

    Update URL in server.js

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const allowedOrigins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:3000",
-    "https://ziplinkio.vercel.app",
+    "https://ziplink-io.vercel.app",
 ];
 
 const io = socketIo(server, {
